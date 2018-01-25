b8d5cafa148fbdeb89125bd863f0dda7cd419f55
julienbsn <julienbsn@protonmail.com>
2018-01-25 14:30:36 +0100

    Fix issues in check_pkgs.sh

--- check_pkgs.sh
+++ check_pkgs.sh
@@ -1,23 +1,23 @@
 check_pkgs(){
   # check_pkgs function ensures that the required packages are already present
   echo "Checking cURL..."
-  if [ "$(dpkg -l curl | grep ^i) 2> /dev/null" ]; then
-    echo "cURL found!"
+  if [ "$(dpkg-query -l curl | grep ^i)" 2> /dev/null ]; then
+    echo "cURL package found!"
   else
-    echo "Installing cURL"
+    echo "Installing cURL..."
     apt install -y curl &> /dev/null
   fi
 
   echo "Checking GnuPG..."
-  if [ "$(dpkg -l gnupg | grep ^i) 2> /dev/null" ]; then
-    echo "GnuPG found!"
+  if [ "$(dpkg-query -l gnupg | grep ^i)" 2> /dev/null ]; then
+    echo "GnuPG package found!"
   else
-    echo "Installing GnuPG"
+    echo "Installing GnuPG..."
     apt install -y gnupg &> /dev/null
   fi
 
   echo "Checking apt-transport-https..."
-  if [ "$(dpkg -l apt-transport-https | grep ^i) 2> /dev/null" ]; then
+  if [ "$(dpkg-query -l apt-transport-https | grep ^i)" 2> /dev/null ]; then
     echo "apt-transport-https package found!"
   else
     echo "Installing apt-transport-https..."
@@ -25,7 +25,7 @@ check_pkgs(){
   fi
 
   echo "Checking debian-archive-keyring..."
-  if [ "$(dpkg -l debian-archive-keyring | grep ^i) 2> /dev/null" ]; then
+  if [ "$(dpkg-query -l debian-archive-keyring | grep ^i)" 2> /dev/null ]; then
     echo "debian-archive-keyring package found!"
   else
     echo "Installing debian-archive-keyring..."
